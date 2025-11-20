bde1458dd51f742ef3a5d32f6853fe1eb483ac19
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-11-20 23:08:23 +0800

    Adds mood check card to the home page
    Introduces a card on the home page that allows users to quickly log their current mood.
    
    This feature provides a simple and visual way for users to track their feelings throughout the day.
    It includes options for neutral, happy, sad, anxious, and frustrated moods.

---
+++ app/(authenticated)/home/_components/MoodCheckCard.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const MoodCheckCard = () => {
+  return (
+    <Card className='flex w-130'>
+      <CardHeader>
+          <CardTitle>How are you feeling?</CardTitle>
+          <CardDescription>
+            Track your mood throughout the day by logging how you feel.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='flex justify-center items-center'>
+          <CardAction>
+            <Button variant="ghost" className='flex flex-col h-max hover:bg-transparent! hover:text-primary hover:scale-105'>
+              <Image
+                src="neutral-feeling.svg"
+                width={50} height={50}
+                alt='Neutral Feeling'
+              />
+              Neutral
+            </Button>
+          </CardAction>
+          <CardAction>
+            <Button variant="ghost" className='flex flex-col h-max hover:bg-transparent! hover:text-primary hover:scale-105'>
+              <Image
+                src="happy-feeling.svg"
+                width={50} height={50}
+                alt='Neutral Feeling'
+              />
+              Happy
+            </Button>
+          </CardAction>
+          <CardAction>
+            <Button variant="ghost" className='flex flex-col h-max hover:bg-transparent! hover:text-primary hover:scale-105'>
+              <Image
+                src="sad-feeling.svg"
+                width={50} height={50}
+                alt='Neutral Feeling'
+              />
+              Sad
+            </Button>
+          </CardAction>
+          <CardAction>
+            <Button variant="ghost" className='flex flex-col h-max hover:bg-transparent! hover:text-primary hover:scale-105'>
+              <Image
+                src="anxious-feeling.svg"
+                width={50} height={50}
+                alt='Neutral Feeling'
+              />
+              Anxious
+            </Button>
+          </CardAction>
+          <CardAction>
+            <Button variant="ghost" className='flex flex-col h-max hover:bg-transparent! hover:text-primary hover:scale-105'>
+              <Image
+                src="frustated-feeling.svg"
+                width={50} height={50}
+                alt='Neutral Feeling'
+              />
+              Frustated
+            </Button>
+          </CardAction>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default MoodCheckCard
