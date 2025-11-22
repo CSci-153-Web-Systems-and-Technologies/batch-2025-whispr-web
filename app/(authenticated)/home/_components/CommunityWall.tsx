63c9fe466b2201fef1fd6fe4b3b46d793e08160e
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-11-23 01:08:30 +0800

    Adds user post input to community wall
    Implements a user input component for the community wall,
    allowing users to share anonymous posts.
    Also introduces a reusable textarea component.

--- app/(authenticated)/home/_components/CommunityWall.tsx
+++ app/(authenticated)/home/_components/CommunityWall.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import PostCard from './PostCard'
+import PostInput from './PostInput'
 
 const CommunityWall = () => {
   return (
     <div className='flex flex-col flex-1 rounded-xl shadow-md'>
       <div className='flex items-center justify-between bg-secondary rounded-t-xl py-4 px-8'>
         <span>Community Feed</span>
-        <span className='bg-primary text-white text-sm py-2 px-5 rounded-full'>Nearby</span>
+        <span className='flex items-center gap-2 text-sm py-2 px-5 rounded-full'>
+          <div className='w-3 h-3 aspect-square bg-primary rounded-full'></div>
+          Nearby
+        </span>
       </div>
       <div className='flex flex-col gap-4 p-4'>
+        <PostInput />
         <PostCard />
         <PostCard />
         <PostCard />
